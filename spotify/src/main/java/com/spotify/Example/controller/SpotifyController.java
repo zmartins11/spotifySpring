3191826787c3d3a29e744eec1484a1023fcea440
zmartins11 <daniel98bm@gmail.com>
2023-08-08 23:47:13 +0100

    refator controller / getTrracks

--- spotify/src/main/java/com/spotify/Example/controller/SpotifyController.java
+++ spotify/src/main/java/com/spotify/Example/controller/SpotifyController.java
@@ -23,33 +23,12 @@ import java.net.URI;
 @RequestMapping("/api")
 public class SpotifyController {
 
-    private static final URI redirecUri = SpotifyHttpManager.makeUri("http://localhost:8080/api/get-user-code/");
-
 private final ApiService apiService;
 
-
-    private String code = "";
-
-    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-            .setClientId(KeysEnum.CLIENT_ID.getKey())
-            .setClientSecret(KeysEnum.CLIENT_SECRET.getKey())
-            .setRedirectUri(redirecUri)
-            .build();
 
     @Autowired
     public SpotifyController(ApiService apiService) {
         this.apiService = apiService;
-    }
-
-    @GetMapping("loginTeste")
-    @ResponseBody
-    public String spotifyLogin() {
-        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
-                .scope("user-read-private, user-read-email, user-top-read")
-                .show_dialog(true)
-                .build();
-        final URI uri = authorizationCodeUriRequest.execute();
-        return uri.toString();
     }
 
     @GetMapping("testFlask")
@@ -57,55 +36,4 @@ private final ApiService apiService;
         return apiService.callFlaskApi();
     }
 
-    @GetMapping(value = "get-user-code")
-    public String getSpotifyUserCode(@RequestParam("code") String userCode, HttpServletResponse response) throws IOException {
-        code = userCode;
-        AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
-
-        try {
-            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
-
-            // Set access and refresh token for further "spotifyApi" object usage
-            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
-            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
-
-            System.out.println("Expires in:" + authorizationCodeCredentials.getExpiresIn());
-        } catch (IOException | SpotifyWebApiException | org.apache.hc.core5.http.ParseException e) {
-
-        }
-
-        response.sendRedirect("http://localhost:4200/top-artists");
-        return spotifyApi.getAccessToken();
-    }
-
-    @GetMapping("/callback")
-    public String callback(@RequestParam("code") String code) {
-        String accessTokenUrl = "https://accounts.spotify.com/api/token";
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("grant_type", "authorization_code");
-        body.add("code", code);
-        body.add("redirect_uri", "http://localhost:8080/callback"); // Replace with your actual redirect URI
-        body.add("client_id", "6b64dd17fbeb44c7aa8eea3fadc6c28f"); // Replace with your actual client ID
-        body.add("client_secret", "17631b9e74f045b29eb245236daf399b"); // Replace with your actual client secret
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(body, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                accessTokenUrl,
-                HttpMethod.POST,
-                entity,
-                String.class
-        );
-
-        // Parse the response and retrieve the access token
-        String accessToken = response.getBody().split("\"access_token\":\"")[1].split("\"")[0];
-
-        return "Your access token: " + accessToken;
-    }
 }
--- spotify/src/main/java/com/spotify/Example/service/ApiService.java
+++ spotify/src/main/java/com/spotify/Example/service/ApiService.java
@@ -1,5 +1,7 @@
 package com.spotify.Example.service;
 
+import com.spotify.Example.model.SavedTrackEntity;
+import com.spotify.Example.repository.SavedTrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -7,18 +9,22 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import se.michaelthelin.spotify.model_objects.specification.SavedTrack;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class ApiService {
 
     private final RestTemplate restTemplate;
+    private final SavedTrackRepository savedTrackRepository;
 
     @Autowired
-    public ApiService(RestTemplate restTemplate) {
+    public ApiService(RestTemplate restTemplate, SavedTrackRepository savedTrackRepository) {
         this.restTemplate = restTemplate;
+        this.savedTrackRepository = savedTrackRepository;
     }
 
     public ResponseEntity<String> callFlaskApi() {
@@ -27,9 +33,16 @@ public class ApiService {
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, String> data = new HashMap<>();
-        data.put("foo fighters", "pretender");
-        data.put("derek trucks", "crow jane");
 
+        int count = 0;
+        List<SavedTrackEntity> savedTrackList = savedTrackRepository.findAll();
+        for(SavedTrackEntity track : savedTrackList) {
+            data.put(track.getArtist(), track.getSongName());
+            count ++;
+            if (count > 5) {
+                break;
+            }
+        }
         HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(data, headers);
 
         ResponseEntity<String> response = restTemplate.postForEntity("http://127.0.0.1:5000/testSpring", requestEntity, String.class);
