687ca11766bb239abc8b56d4f7a8a4dd37b3b22f
zmartins11 <daniel98bm@gmail.com>
2023-08-10 00:06:27 +0100

    mp4 to mp3

--- spotify/src/main/java/com/spotify/Example/service/SpotifyService.java
+++ spotify/src/main/java/com/spotify/Example/service/SpotifyService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import se.michaelthelin.spotify.model_objects.specification.ArtistSimplified;
 import se.michaelthelin.spotify.model_objects.specification.SavedTrack;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 @Service
@@ -36,19 +38,36 @@ public class SpotifyService {
     }
 
     public void formatMp4() {
-        String inputFilePath = "D:\\codex-cuphead\\obsleague.mp4";
-		String outputFilePath = "D:\\codex-cuphead\\output_audio.mp3";
-		try {
-			// Use ffmpeg to convert the MP4 file to MP3
-			String ffmpegPath = "C:\\ffmpeg\\bin\\ffmpeg.exe";
-			ProcessBuilder processBuilder = new ProcessBuilder(ffmpegPath, "-i", inputFilePath, outputFilePath);
-			Process process = processBuilder.start();
+        String inputFolder = "D:\\codex-cuphead";
+		String outpuFolder = "D:\\songsFormatMP3";
+        String ffmpegPath = "C:\\ffmpeg\\bin\\ffmpeg.exe";
+
+        File inputDir = new File(inputFolder);
+        File [] videoFiles = inputDir.listFiles((dir, name) -> name.endsWith(".mp4"));
 
 
-			System.out.println("MP4 to MP3 conversion completed successfully.");
-			String tst = "tes";
-		} catch (Exception e ) {
-			e.printStackTrace();
-		}
+        if (videoFiles != null) {
+            for (File videoFile : videoFiles) {
+                String inputFilePath = videoFile.getAbsolutePath();
+                String outputFilePath = outpuFolder + "\\" + videoFile.getName().replace(".mp4", ".mp3");
+
+                try {
+                    ProcessBuilder processBuilder = new ProcessBuilder(ffmpegPath, "-i", inputFilePath, outputFilePath);
+                    Process process = processBuilder.start();
+                    int exitCode = process.waitFor();
+
+                    if(exitCode == 0) {
+                        System.out.println("Convertion of " + videoFile.getName() + " completed successfully");
+                    } else {
+                        System.out.println("Convertion of " + videoFile.getName() + " failed with exit code: " + exitCode);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+         }
+
     }
 }
